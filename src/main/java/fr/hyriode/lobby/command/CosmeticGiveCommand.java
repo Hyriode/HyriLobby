6c5fa11bf1ec23850bcdb3598a1b43fc48e82f60
Pas2Pseudo <williamjolivet74@gmail.com>
2023-04-22 14:09:06 +0200

    Update CosmeticGiveCommand.java

--- src/main/java/fr/hyriode/lobby/command/CosmeticGiveCommand.java
+++ src/main/java/fr/hyriode/lobby/command/CosmeticGiveCommand.java
@@ -1,6 +1,8 @@
 package fr.hyriode.lobby.command;
 
+import fr.hyriode.api.language.HyriLanguage;
 import fr.hyriode.api.player.IHyriPlayer;
+import fr.hyriode.api.player.IHyriPlayerSession;
 import fr.hyriode.api.rank.StaffRank;
 import fr.hyriode.cosmetics.HyriCosmetics;
 import fr.hyriode.cosmetics.common.Cosmetic;
@@ -10,7 +12,12 @@ import fr.hyriode.hyrame.command.CommandInfo;
 import fr.hyriode.hyrame.command.CommandUsage;
 import fr.hyriode.hyrame.command.HyriCommand;
 import fr.hyriode.hyrame.language.HyrameMessage;
+import fr.hyriode.hyrame.npc.NPC;
+import fr.hyriode.hyrame.npc.NPCManager;
 import fr.hyriode.lobby.HyriLobby;
+import fr.hyriode.lobby.npc.LobbyNPCHandler;
+import fr.hyriode.lobby.player.LobbyPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class CosmeticGiveCommand extends HyriCommand<HyriLobby> {
@@ -46,11 +53,12 @@ public class CosmeticGiveCommand extends HyriCommand<HyriLobby> {
                 return;
             }
 
-            final IHyriPlayer account = IHyriPlayer.get(target.getUniqueId());
-            account.getTransactions().add(CosmeticTransaction.TYPE, new CosmeticTransaction(cosmetic.getId()));
-            account.update();
-
-            player.sendMessage("§aCosmetic given to " + target.getName() + " " + cosmetic.getTranslatedName().getValue(player));
+            Bukkit.getScheduler().runTaskLater(this.plugin, () -> {
+                final IHyriPlayer account = IHyriPlayer.get(target.getUniqueId());
+                account.getTransactions().add(CosmeticTransaction.TYPE, new CosmeticTransaction(cosmetic.getId()));
+                account.update();
+                player.sendMessage("§aCosmetic given to " + target.getName() + " " + cosmetic.getTranslatedName().getValue(player));
+            }, 5L);
         });
 
         super.handle(ctx);
